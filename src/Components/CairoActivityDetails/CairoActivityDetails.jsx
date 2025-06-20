e81f58bceb80684d0cdcb0fbbae6cd8ee4d9034a
Abdallah khodari <abdallahkhodari35@gmail.com>
2025-06-20 16:38:34 +0300

    Update CairoActivityDetails.jsx

--- src/Components/CairoActivityDetails/CairoActivityDetails.jsx
+++ src/Components/CairoActivityDetails/CairoActivityDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import pyramids from "../../assets/pyramids.jpg";
+import pyramids from "../../assets/luxor.jpg";
 
 const activities = [
   {
